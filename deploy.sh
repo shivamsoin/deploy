7f3fd5a8942f81cb9df4db3bf435dca8fd7ed36c
shivamsoin <68854577+shivamsoin@users.noreply.github.com>
2021-04-06 23:57:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Setting Up Dependencies"
 sudo apt-get update
-sudo ufw allow 'Nginx Full' # nginx setup
+sudo ufw allow 'Nginx Full' --assume-yes # nginx setup
 sudo apt upgrade
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install nodejs --assume-yes
@@ -19,6 +19,10 @@ git clone https://github.com/shivamsoin/test.git
 cd test
 pm2 start index.js
 pm2 status 
+pm2 startup ubuntu
+echo "Testing App on Local Host"
+curl http://localhost:8080
+
 
 
 
