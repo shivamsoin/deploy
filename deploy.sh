38f478981596864a46f1b10608e306a6ca0142c6
shivamsoin <68854577+shivamsoin@users.noreply.github.com>
2021-04-06 23:16:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
+echo "Setting Up Dependencies"
+sudo apt-get update
+sudo ufw allow 'Nginx Full' # nginx setup
+sudo apt upgrade
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install nodejs --assume-yes
+# checking if its working or not
+node --version 
+npm --version
+sudo apt-get install git --assume-yes #Git 
+sudo npm install pm2 -g #Pm2
+sudo apt update && upgrade
+sudo apt install apache2 -assume-yes
+systemctl start apache2
+systemctl enable apache2
+apt-get install build-essential --assume-yes
+git clone https://github.com/shivamsoin/test.git
+cd test
+pm2 start index.js
+pm2 status 
 
 
-# Please make sure you are root user before executing this script
-echo "Enter git repo Link : "
-read link
-echo "Enter repo name : "
-read repo
-cd /var/www/html
-rm *
-git clone $link
-cd $repo
-mv * ..
-systemctl reload apache2
+
+
+
+
+
+
+
+
+
+
+
